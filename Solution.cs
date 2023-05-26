991b5bc43274d59422174caeadbd49bd32cefc2b
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-05-26 06:04:33 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,26 @@
+
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] Intersection(int[] firstInput, int[] secondInput)
+    {
+        HashSet<int> presentInFirstInput = new HashSet<int>();
+        foreach (var num in firstInput)
+        {
+            presentInFirstInput.Add(num);
+        }
+
+        List<int> intersection = new List<int>();
+        foreach (var num in secondInput)
+        {
+            if (presentInFirstInput.Remove(num))
+            {
+                intersection.Add(num);
+            }
+        }
+
+        return intersection.ToArray();
+    }
+}
